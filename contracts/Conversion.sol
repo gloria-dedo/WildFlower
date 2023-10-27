8f3efc039a6e051d78b81be3c21778bf43abe471
Gloria Dedo <79916666+gloria-dedo@users.noreply.github.com>
2023-10-27 05:18:37 +0000

    Update Conversion.sol

--- contracts/Conversion.sol
+++ contracts/Conversion.sol
@@ -1,59 +1,81 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./Ecedi.sol";
-contract Conversion is Ownable { 
-    // The token converted from
-    ERC20 public tokenFrom;
+import "./Currency.sol";
 
-    // The token converted to
-   Ecedes public tokenTo;
+contract MyContract is Ownable {
+    IERC20 public eCedi;
+    IERC20 public eNaira;
 
-    // Address where cash is collected
-    address payable public wallet;
+    event Deposit(uint amount, string currency, address user);
+    event Withdrawal(uint amount, string currency, address user);
+    event Conversion(
+        string from,
+        string to,
+        uint fromAmount,
+        uint toAmount,
+        address user
+    );
 
-    constructor( address payable _wallet, ERC20 _tokenTo, ERC20 _tokenFrom) {
-        require(_wallet != address(0));
-
-        // SETUP WALLET AND TOKENS
-        wallet = _wallet;
-        tokenTo = _tokenTo;
-        tokenFrom = _tokenFrom;
+    constructor() Ownable(msg.sender) {
+        eCedi = new Currency("eCedi", "UGHS");
+        eNaira = new Currency("eNaira", "NGNS");
     }
 
+    function deposit(uint256 amount, string memory currency) public {
+        IERC20 token;
+        if (keccak256(bytes(currency)) == keccak256(bytes("GHS"))) {
+            token = eCedi;
+        } else if (keccak256(bytes(currency)) == keccak256(bytes("NGN"))) {
+            token = eNaira;
+        } else {
+            revert("Invalid currency");
+        }
+        token.transferFrom(address(this), msg.sender, amount);
 
-    function convert( address _from) payable public {
-        require(_from != address(0));
-        require(msg.value > 0);
-tokenTo.mint(_from, msg.value);
-        tokenFrom.transfer(msg.value);
-        
-
+        emit Deposit(amount, currency, msg.sender);
     }
 
-     //function withdraw(uint amount) external onlyOwner {
-       // require(balance >= amount, "Insufficient funds");
+    function withdraw(uint256 amount, string memory currency) public onlyOwner {
+        IERC20 token;
+        if (keccak256(bytes(currency)) == keccak256(bytes("GHS"))) {
+            token = eCedi;
+        } else if (keccak256(bytes(currency)) == keccak256(bytes("NGN"))) {
+            token = eNaira;
+        } else {
+            revert("Invalid currency");
+        }
+        token.transferFrom(msg.sender, address(this), amount);
 
-
-        
-        //(bool success, ) = msg.sender.call{value: amount}("");
-        //require(success, "Transfer failed");
-    //}
-    function withdraw(uint amount) external onlyOwner {
-    require(address(this).balance >= amount, "Insufficient funds");
-
-    (bool success, ) = wallet.call{value: amount}("");
-    require(success, "Transfer failed");
-}
-
-
-
-    // Function to check the contract's Ether balance
-    function getBalance() external view override returns (uint256) {
-        return address(this).balance;
+        emit Withdrawal(amount, currency, msg.sender);
     }
 
+    function convert(
+        uint256 amount,
+        string memory from,
+        string memory to
+    ) public {
+        IERC20 tokenFrom;
+        IERC20 tokenTo;
+        if (keccak256(bytes(from)) == keccak256(bytes("GHS"))) {
+            tokenFrom = eCedi;
+        } else if (keccak256(bytes(from)) == keccak256(bytes("NGN"))) {
+            tokenFrom = eNaira;
+        } else {
+            revert("Invalid currency");
+        }
+        if (keccak256(bytes(to)) == keccak256(bytes("GHS"))) {
+            tokenTo = eCedi;
+        } else if (keccak256(bytes(to)) == keccak256(bytes("NGN"))) {
+            tokenTo = eNaira;
+        } else {
+            revert("Invalid currency");
+        }
+        tokenFrom.transferFrom(msg.sender, address(this), amount);
+        tokenTo.transfer(msg.sender, amount);
+
+        emit Conversion(from, to, amount, amount, msg.sender);
+    }
 }
